53f22aa414d22149dbacb46ca0fffff78d952569
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-26 13:46:35 +0200

    Create Home.jsx

---
+++ front/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header/Header";
+
+const Home = () => {
+  return (
+    <div className="w-svw h-svh bg-slate-100">
+      <Header />
+      <h1 className="font-bold text-4xl">Restaurants in Salamanca</h1>
+    </div>
+  );
+};
+
+export default Home;
