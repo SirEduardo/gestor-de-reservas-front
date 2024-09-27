32b8f5f234bc91d5fbe63a3f574a79678d1294d0
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-27 11:47:24 +0200

    Create Cards.jsx

---
+++ front/src/components/Cards/Cards.jsx
@@ -0,0 +1,42 @@
+const capitalize = (str) => {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const Cards = ({
+  id,
+  img,
+  name,
+  average_rating,
+  rating_number,
+  category,
+  location,
+  opening,
+  closing,
+}) => {
+  return (
+    <div
+      className="relative flex flex-col bg-slate-300 gap-3 rounded-xl cursor-pointer "
+      key={id}
+    >
+      <img
+        className="w-full h-56 object-cover rounded-t-xl"
+        src={img}
+        alt={name}
+      ></img>
+      <div className=" flex flex-col gap-2 p-3">
+        <h2 className="font-bold">{capitalize(name)}</h2>
+        <p>
+          {average_rating} {rating_number}
+        </p>
+        <p>{capitalize(category)}</p>
+        <p>{capitalize(location)}</p>
+        <p>
+          Horario: {opening} - {closing}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
