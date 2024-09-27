d39d32b7c59edbcd9f5d240adcd4d4ce5d1b09a8
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-27 11:47:22 +0200

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Auth from "./pages/Login/auth";
+import Restaurants from "./pages/Restaurants/Restaurants";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/restaurants/:id" element={<Restaurants />} />
       </Routes>
     </div>
   );
