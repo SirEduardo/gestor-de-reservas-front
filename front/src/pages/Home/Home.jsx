29ac1a08b5b2800a69e2df64982f09c589918036
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-09-27 11:47:29 +0200

    Update Home.jsx

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
+import { Link } from "react-router-dom";
+import Cards from "../../components/Cards/Cards";
 
 const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -7,31 +9,37 @@ const Home = () => {
   const getRestaurants = async () => {
     const response = await fetch("http://localhost:3000/api/v1/restaurants");
     const res = await response.json();
+    console.log(res);
+
     setRestaurants(res);
   };
 
   useEffect(() => {
     getRestaurants(restaurants);
-  }, [getRestaurants]);
+  }, []);
 
   return (
     <div className="w-svw h-svh bg-slate-100">
       <Header />
-      <h1 className="font-bold text-4xl">Restaurants in Salamanca</h1>
-      <div className="flex justify-center flex-col items-center gap-5">
+      <h1 className="font-bold text-4xl mt-10 pl-4">
+        Restaurants in Salamanca
+      </h1>
+      <div className="flex justify-center items-center gap-5 mt-20">
         {Array.isArray(restaurants) ? (
           restaurants.map((res) => (
-            <div
-              className="flex flex-col bg-slate-300 p-20 gap-3 rounded-xl cursor-pointer"
-              key={res._id}
-            >
-              <h2 className="font-bold">{res.name}</h2>
-              <p>{res.average_rating}</p>
-              <p>{res.category}</p>
-              <p>
-                {res.schedule.opening} - {res.schedule.closing}
-              </p>
-            </div>
+            <Link key={res._id} to={`/restaurants/${res._id}`}>
+              <Cards
+                key={res._id}
+                name={res.name}
+                id={res._id}
+                img={res.img}
+                category={res.category}
+                average_rating={res.average_rating}
+                rating_number={res.rating_number}
+                opening={res.schedule.opening}
+                closing={res.schedule.closing}
+              />
+            </Link>
           ))
         ) : (
           <p>No hay restaurantes disponibles</p>
