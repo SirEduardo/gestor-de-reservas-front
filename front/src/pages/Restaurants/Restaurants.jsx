5182ca1a1079982b728db9adc9fcd1939d2befae
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-01 14:40:09 +0200

    Update Restaurants.jsx

--- front/src/pages/Restaurants/Restaurants.jsx
+++ front/src/pages/Restaurants/Restaurants.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Rating from "../../components/Rating/Rating";
 import { Header } from "../../components/Header/Header";
 
@@ -25,18 +25,21 @@ const Restaurants = () => {
   }, [id]);
 
   return (
-    <div className="w-screen h-screen bg-slate-100">
+    <div className="min-h-screen flex flex-col bg-slate-200">
       <Header />
-      <div className="w-svw h-max bg-slate-100 flex justify-center">
-        <div className="flex gap-5 pt-14 pb-14 w-2/4 justify-center">
+      <div className="flex-grow flex justify-center py-14">
+        <div className="w-2/4 max-w-4xl">
           {restaurant ? (
             <div className="flex flex-col gap-5">
               <h1 className="font-bold text-3xl">{restaurant.name}</h1>
               <div>
-                <img className="rounded-xl" src={restaurant.img}></img>
+                <img
+                  className="rounded-xl w-full h-auto"
+                  src={restaurant.img}
+                ></img>
               </div>
-              <section className="flex justify-between gap-5">
-                <div className="flex flex-col w-full p-6 bg-white rounded-lg border-slate-300 border gap-3">
+              <section className="flex flex-col md:flex-row justify-between gap-5">
+                <div className="flex flex-col w-full p-6 bg-white rounded-lg border gap-3">
                   <h3 className="font-bold text-lg">
                     Puntuaciones y opiniones
                   </h3>
@@ -45,7 +48,7 @@ const Restaurants = () => {
                     rating_number={restaurant.rating_number}
                   />
                 </div>
-                <div className="flex flex-col w-full bg-white p-6 rounded-lg border-slate-300 border gap-3">
+                <div className="flex flex-col w-full bg-white p-6 rounded-lg border gap-3">
                   <h3 className="font-bold text-lg">Detalles</h3>
                   <div>
                     <h4 className="font-semibold">Horario</h4>
@@ -60,12 +63,20 @@ const Restaurants = () => {
                     <div>{restaurant.category}</div>
                   </div>
                 </div>
-                <div className="flex flex-col w-full bg-white p-6 rounded-lg border-slate-300 border  gap-3">
+                <div className="flex flex-col w-full bg-white p-6 rounded-lg border  gap-3">
                   <h3 className="font-bold text-lg">Ubicacion y contacto</h3>
                   <span>Contacto: {restaurant.telephone}</span>
                   {restaurant.location}
                 </div>
               </section>
+              <div className="bg-white p-5 rounded-lg">
+                <h4 className="font-bold text-xl pb-10">Contribuye!</h4>
+                <Link to="/comments">
+                  <button className="bg-black text-white px-5 py-3 rounded-full font-bold">
+                    Escribe un comentario
+                  </button>
+                </Link>
+              </div>
             </div>
           ) : (
             <p>No hay restaurantes disponibles</p>
