919c8916f77e2900e356893bf7bf28809f38870d
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-01 14:39:53 +0200

    Update Header.jsx

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -11,39 +11,43 @@ export const Header = () => {
   };
 
   return (
-    <header className="h-24 flex items-center justify-center border-b-2">
-      <nav className="w-full">
-        <ul className="w-full flex items-center justify-around font-semibold">
-          <NavLink to="/">
-            <h3 className="text-xl">TripAdvisor</h3>
-          </NavLink>
-          {user ? (
-            <div className="flex items-center justify-evenly w-1/4">
-              <span>Bienvenido, {user}</span>
-              <NavLink
-                to={role === "admin" ? "/myRestaurant" : "/myReservations"}
-              >
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-4">
+        <nav className="flex items-center justify-between h-16 md:h-20">
+          <ul className="w-full flex items-center justify-around font-semibold">
+            <NavLink to="/">
+              <h3 className="text-2xl font-bold text-gray-800 hover:text-gray-600 transition-colors">
+                TripAdvisor
+              </h3>
+            </NavLink>
+            {user ? (
+              <div className="flex items-center justify-evenly w-1/4">
+                <span>Bienvenido, {user}</span>
+                <NavLink
+                  to={role === "admin" ? "/myRestaurant" : "/myReservations"}
+                >
+                  <button className="bg-black hover:bg-gray-900 text-white rounded-full p-3">
+                    {role === "admin" ? "Mi Restaurante" : "Mis Reservas"}
+                  </button>
+                </NavLink>
+
+                <button
+                  onClick={handleLogout}
+                  className="bg-black hover:bg-gray-900 text-white rounded-full py-3 px-5"
+                >
+                  Salir
+                </button>
+              </div>
+            ) : (
+              <NavLink to="/auth">
                 <button className="bg-black text-white rounded-full p-3">
-                  {role === "admin" ? "Mi Restaurante" : "Mis Reservas"}
+                  Iniciar sesion
                 </button>
               </NavLink>
-
-              <button
-                onClick={handleLogout}
-                className="bg-black text-white rounded-full py-3 px-5"
-              >
-                Salir
-              </button>
-            </div>
-          ) : (
-            <NavLink to="/auth">
-              <button className="bg-black text-white rounded-full p-3">
-                Iniciar sesion
-              </button>
-            </NavLink>
-          )}
-        </ul>
-      </nav>
+            )}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
