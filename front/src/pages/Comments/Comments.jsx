325891fafc16d936327b9ef8599b1ee611ab27ed
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-01 14:40:04 +0200

    Create Comments.jsx

---
+++ front/src/pages/Comments/Comments.jsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header/Header";
+import Rate from "../../components/Rating/Rate";
+
+const Comments = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Rate />
+      </main>
+    </div>
+  );
+};
+
+export default Comments;
