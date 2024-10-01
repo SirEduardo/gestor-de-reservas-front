40f828800b433d67d5675b1da60b7806cb89c3e3
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-01 21:06:51 +0200

    Update logout.js

--- front/src/utils/Functions/logout.js
+++ front/src/utils/Functions/logout.js
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+const navigate = useNavigate;
 export const logout = () => {
   const confirmation = confirm("sure you want to logout");
   if (confirmation) {
     localStorage.removeItem("user");
     localStorage.removeItem("token");
+    localStorage.removeItem("id");
   }
+  navigate("/");
 };
