5bee7fbdd2e41d6154995310edb87b3efb9ff03f
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-01 13:10:31 +0200

    Update Home.jsx

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -2,21 +2,35 @@ import { useEffect, useState } from "react";
 import { Header } from "../../components/Header/Header";
 import { Link } from "react-router-dom";
 import Cards from "../../components/Cards/Cards";
-import Search from "../../components/Search/Search";
+import SearchByName from "../../components/Search/SearchByName";
+import SearchByCategory from "../../components/Search/SearchByCategory";
 
 const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   const getRestaurants = async () => {
-    const response = await fetch("http://localhost:3000/api/v1/restaurants");
-    const res = await response.json();
-
-    setRestaurants(res);
+    try {
+      const response = await fetch("http://localhost:3000/api/v1/restaurants");
+      if (!response.ok) {
+        throw new Error("Error fetching restaurants");
+      }
+      const res = await response.json();
+      setRestaurants(res);
+      setAllRestaurants(res);
+      const uniqueCategories = Array.from(
+        new Set(res.map((restaurant) => restaurant.category))
+      );
+      setCategories(uniqueCategories);
+    } catch (error) {
+      console.error("Failed to fetch restaurants:", error);
+    }
   };
 
   useEffect(() => {
     getRestaurants(restaurants);
-  });
+  }, []);
 
   return (
     <div className="w-screen h-screen bg-slate-100">
@@ -25,15 +39,25 @@ const Home = () => {
         <h1 className="font-bold text-4xl mt-10 pl-4">
           Descubre tu próximo restaurante favorito
         </h1>
-        <Search />
+        <div className="flex items-center">
+          <SearchByName
+            setRestaurants={setRestaurants}
+            categories={categories}
+          />
+          <SearchByCategory
+            setRestaurants={setRestaurants}
+            restaurants={allRestaurants}
+            categories={categories}
+          />
+        </div>
       </div>
 
       <div className="flex flex-col justify-center items-center gap-5 mt-20">
-        <div className="flex flex-wrap justify-center gap-5">
+        <div className="flex flex-wrap justify-center gap-5 px-5 ">
           {Array.isArray(restaurants) ? (
             restaurants.map((res) => (
               <Link key={res._id} to={`/restaurants/${res._id}`}>
-                <div className="w-64 h-80 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105">
+                <div className="w-64 h-80 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 mb-24">
                   <Cards
                     key={res._id}
                     name={res.name}
