2a791e173eef85f09a6e1ebb00310c5ddbac3d4e
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-02 15:03:34 +0200

    Update Header.jsx

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -14,7 +14,6 @@ export const Header = () => {
   const handleLogout = () => {
     logout();
     navigate("/");
-    window.location.reload();
   };
 
   return (
@@ -45,12 +44,12 @@ export const Header = () => {
                   Bienvenido, {user}
                 </li>
                 <li>
-                  <NavLink
+                  {/*<NavLink
                     to={role === "admin" ? "/myRestaurant" : "/myReservations"}
                     className="bg-black hover:bg-gray-900 text-white rounded-full py-2 px-4 text-sm md:text-base font-semibold"
                   >
                     {role === "admin" ? "Mi Restaurante" : "Mis Reservas"}
-                  </NavLink>
+                  </NavLink>*/}
                 </li>
                 <li>
                   <button
