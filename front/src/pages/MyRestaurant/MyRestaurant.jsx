efc8d455090d16e2cea76617483cd701e5557580
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-03 12:16:01 +0200

    Update MyRestaurant.jsx

--- front/src/pages/MyRestaurant/MyRestaurant.jsx
+++ front/src/pages/MyRestaurant/MyRestaurant.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { Header } from "../../components/Header/Header";
+import RestaurantModal from "../../components/RestaurantModal/RestaurantModal";
+
 const MyRestaurant = () => {
-  return <div>MyRestaurants</div>;
+  const [modal, setModal] = useState(false);
+
+  return (
+    <div>
+      <Header />
+      <div className="min-h-screen bg-gray-50">
+        <div className="flex justify-center">
+          <button
+            onClick={() => setModal(!modal)}
+            className="bg-black text-white hover:bg-primary/90 px-6 py-3 rounded-full font-bold transition-colors"
+          >
+            Crea tu restaurante
+          </button>
+          {modal ? <RestaurantModal /> : ""}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyRestaurant;
