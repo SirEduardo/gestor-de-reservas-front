8f0b9ebec815b65cead0e3f34b0a393c2a9fd75b
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-03 15:16:12 +0200

    Update MyRestaurant.jsx

--- front/src/pages/MyRestaurant/MyRestaurant.jsx
+++ front/src/pages/MyRestaurant/MyRestaurant.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "../../components/Header/Header";
 import RestaurantModal from "../../components/RestaurantModal/RestaurantModal";
+import { AnimatePresence } from "framer-motion";
 
 const MyRestaurant = () => {
   const [modal, setModal] = useState(false);
@@ -9,14 +10,16 @@ const MyRestaurant = () => {
     <div>
       <Header />
       <div className="min-h-screen bg-gray-50">
-        <div className="flex justify-center">
+        <div className="flex flex-col items-center justify-center pt-20">
           <button
             onClick={() => setModal(!modal)}
             className="bg-black text-white hover:bg-primary/90 px-6 py-3 rounded-full font-bold transition-colors"
           >
             Crea tu restaurante
           </button>
-          {modal ? <RestaurantModal /> : ""}
+          <AnimatePresence>
+            {modal ? <RestaurantModal setModal={setModal} /> : ""}
+          </AnimatePresence>
         </div>
       </div>
     </div>
