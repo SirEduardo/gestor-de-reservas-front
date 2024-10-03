7a61aa67cffdab9983694c0a2234cf52764e362a
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-03 17:43:16 +0200

    Update logout.js

--- front/src/utils/Functions/logout.js
+++ front/src/utils/Functions/logout.js
@@ -2,6 +2,7 @@ export const logout = () => {
   const confirmation = confirm("sure you want to logout");
   if (confirmation) {
     localStorage.removeItem("user");
+    localStorage.removeItem("role");
     localStorage.removeItem("token");
     localStorage.removeItem("id");
   }
