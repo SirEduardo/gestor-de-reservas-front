e6870eb518e6d00df14786be1c08c0e241531656
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-04 10:46:53 +0200

    Update MyRestaurant.jsx

--- front/src/pages/MyRestaurant/MyRestaurant.jsx
+++ front/src/pages/MyRestaurant/MyRestaurant.jsx
@@ -22,9 +22,6 @@ const MyRestaurant = () => {
       const userData = response.data;
       const restaurant = userData.restaurant;
       const restaurantId = restaurant[0]?._id;
-      console.log(restaurant);
-
-      console.log(restaurantId);
 
       if (!restaurantId) {
         console.log("No restaurant found for this user");
@@ -49,34 +46,39 @@ const MyRestaurant = () => {
       <Header />
       <div className="min-h-screen bg-gray-50">
         <div className="flex flex-col items-center justify-center pt-20">
-          <button
-            onClick={() => setModal(!modal)}
-            className="bg-black text-white hover:bg-primary/90 px-6 py-3 rounded-full font-bold transition-colors"
-          >
-            Crea tu restaurante
-          </button>
+          {!restaurant && (
+            <button
+              onClick={() => setModal(!modal)}
+              className="bg-black text-white hover:bg-primary/90 px-6 py-3 rounded-full font-bold transition-colors"
+            >
+              Crea tu restaurante
+            </button>
+          )}
           <AnimatePresence>
-            {modal ? <RestaurantModal setModal={setModal} /> : ""}
+            {modal ? <RestaurantModal setModal={setModal} /> : null}
           </AnimatePresence>
         </div>
         {restaurant ? (
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="flex items-center justify-center">
             <div
               key={restaurant._id}
               className="bg-white overflow-hidden shadow-lg rounded-lg"
             >
-              <div className="px-4 py-5 sm:p-6">
+              <div className="px-4 py-5 sm:p-6 flex flex-col gap-4">
                 <h3 className="text-lg font-semibold text-gray-900 mb-2">
                   {restaurant.name}
                 </h3>
-                <p className="text-gray-600 mb-4">{restaurant.name}</p>
+                <img className="rounded-lg w-96" src={restaurant.img}></img>
+                <p className="text-gray-600">{restaurant.opening}</p>
+                <p className="text-gray-600">{restaurant.closing}</p>
+                <p className="text-sm text-gray-500">{restaurant.category}</p>
                 <p className="text-sm text-gray-500">{restaurant.location}</p>
               </div>
             </div>
           </div>
         ) : (
           <div className="text-center">
-            <p className="text-xl font-semibold text-gray-700">
+            <p className="text-xl font-semibold text-gray-700 mt-20">
               No restaurants found. Create one to get started!
             </p>
           </div>
