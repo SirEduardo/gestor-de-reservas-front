617bf6d78780987d72274703fcdd09e28e81842c
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-05 13:18:53 +0200

    Update Loading.jsx

--- front/src/components/Loading/Loading.jsx
+++ front/src/components/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = ({ message = "Loading data..." }) => {
   return (
     <div className="flex items-center justify-center gap-2">
-      <p className="text-white text-lg">{message}</p>
+      <p className="text-gray-500 text-lg">{message}</p>
       <div className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-blue-500"></div>
     </div>
   );
