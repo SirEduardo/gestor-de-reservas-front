9b78b272b65bf42f882c831128292fdbda91f811
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-24 14:28:40 +0200

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,19 +6,25 @@ import MyReservations from "./pages/MyReservations/MyReservations";
 import Comments from "./components/Create/CreateComments/Comments";
 import Register from "./pages/SingIn/Register";
 import CreateReservation from "./components/Create/CreateReservation/CreateReservation";
+import { GlobalProvider } from "./utils/Hooks/useReducer";
 
 function App() {
   return (
     <div className="h-svh">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Register />} />
-        <Route path="/restaurants/:id" element={<Restaurants />} />
-        <Route path="/myRestaurant" element={<MyRestaurant />} />
-        <Route path="/myReservations" element={<MyReservations />} />
-        <Route path="/comments/:id" element={<Comments />} />
-        <Route path="/createReservation/:id" element={<CreateReservation />} />
-      </Routes>
+      <GlobalProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Register />} />
+          <Route path="/restaurants/:id" element={<Restaurants />} />
+          <Route path="/myRestaurant" element={<MyRestaurant />} />
+          <Route path="/myReservations" element={<MyReservations />} />
+          <Route path="/comments/:id" element={<Comments />} />
+          <Route
+            path="/createReservation/:id"
+            element={<CreateReservation />}
+          />
+        </Routes>
+      </GlobalProvider>
     </div>
   );
 }
