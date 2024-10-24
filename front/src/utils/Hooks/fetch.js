0d479a5e3efd3a25b474ca1a73b11cd2283a0178
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-10-24 14:29:43 +0200

    Update fetch.js

--- front/src/utils/Hooks/fetch.js
+++ front/src/utils/Hooks/fetch.js
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import axios from "axios";
 
 const useFetch = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   const fetchData = async (url, options = {}) => {
-    setLoading(true);
-    setError(null);
-
     try {
       const response = await axios(url, options);
       return response.data;
     } catch (err) {
       console.error("Error fetching data:", err);
-      setError("Ocurrió un error al obtener la información.");
+
       return null;
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -28,7 +19,7 @@ const useFetch = () => {
   const putData = async (url, data, options = {}) => {
     return fetchData(url, { method: "PUT", data, ...options });
   };
-  return { loading, error, fetchData, postData, putData };
+  return { fetchData, postData, putData };
 };
 
 export default useFetch;
